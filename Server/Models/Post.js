ee298eb3c4109f5d7abb425d0c563fb70d2d906b
Manoj Bastakoti <manojbastakoti252@gmail.com>
2023-07-16 18:41:17 +0545

    post schema created

---
+++ Server/Models/Post.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    viewed: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const PostModel = mongoose.model("post", postSchema);
+
+module.exports = PostModel;
